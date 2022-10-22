41c049c8a7b5c19519ce4a2be17ab69a86eb85d9
hnz989 <zhenghualin2008@gmail.com>
2022-10-21 23:40:30 -0400

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by HuaLin Zheng on 10/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
